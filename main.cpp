b961828aef8410e7f26859979ce56a9a0f4e6d96
alexstradan4enkov <alexstradan4enkov@gmail.com>
2025-12-01 13:54:51 +0300

    5) добавлен ввод данных пользователем и main ф-я

--- main.cpp
+++ main.cpp
@@ -9,12 +9,42 @@ int findMax(const vector<int>& numbers) {
     if (numbers.empty()) {
         return numeric_limits<int>::min();
     }
-
-    int max = numbers[0];
+        int max = numbers[0];
     for (size_t i = 1; i < numbers.size(); i++) {
         if (numbers[i] > max) {
             max = numbers[i];
         }
     }
     return max;
+
+}
+
+int main() {
+    int count;
+    cout << "Введите количество элементов в массиве: ";
+    cin >> count;
+
+    if (count <= 0) {
+        cout << "Количество элементов должно быть положительным!" << endl;
+        return 1;
+    }
+
+    vector<int> numbers(count);
+    cout << "Введите элементы массива:" << endl;
+
+    for (int i = 0; i < count; i++) {
+        cout << "Элемент " << (i + 1) << ": ";
+        cin >> numbers[i];
+    }
+
+    int maxValue = findMax(numbers);
+    if (maxValue != numeric_limits<int>::min()) {
+        cout << "Максимальное значение: " << maxValue << endl;
+    }
+    else {
+        cout << "Вектор пуст!" << endl;
+    }
+
+    return 0;
+
 }
